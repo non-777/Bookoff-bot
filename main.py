71bc26ef6b91d96877bd6894b6f3ef763ba1e91f
non-777 <omaeomonnaide@gmail.com>
2026-02-01 05:19:31 +0900

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from flask import Flask, request, jsonify
-import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def health():
-return   "OK"
+def health　():
+return "OK"
 
 @app.route("/update_bookoff_stock", methods=["POST"])
 def update_bookoff_stock():
 data = request.json
 return jsonify({"status": "ok", "received": data})
-
-# ↓ これはローカル用だけ残す
-if __name__ == "__main__":
-port = int(os.environ.get("PORT", 8080))
-app.run(host="0.0.0.0", port=port)
