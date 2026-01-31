e44ddf7194cc97c046ee63ea604b0dd85ddacf2d
non-777 <omaeomonnaide@gmail.com>
2026-02-01 03:30:29 +0900

    main.py

--- main.py
+++ main.py
@@ -17,10 +17,3 @@ return jsonify({
 "url": url,
 "name": name
 })
-
-
-# Cloud Run 用（必須）
-if __name__ == "__main__":
-import os
-port = int(os.environ.get("PORT", 8080))
-app.run(host="0.0.0.0", port=port)
