9c0520b2d7b1300f149cb98e10b02aa3a4228bbc
non-777 <omaeomonnaide@gmail.com>
2026-02-01 04:21:20 +0900

    main.py

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ return "OK"
 @app.route("/update_bookoff_stock", methods=["POST"])
 def update_bookoff_stock():
 data = request.json
-return jsonify({
-"status": "success",
-"received": data
-})
+return jsonify({"status": "ok", "received": data})
 
 if __name__ == "__main__":
 app.run(host="0.0.0.0", port=8080)
